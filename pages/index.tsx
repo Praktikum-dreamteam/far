9eff872a110752be0b52d5eace2adccb882c695a
Nikolay Sidorov <sidorov.n@idaproject.com>
2021-03-28 15:48:32 +0300

    add media mixins

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import Header from '../src/components/Header'
 
 export default function Home() {
   return (
     <div className="home">
-      
+
       <Header/>
 
     </div>
