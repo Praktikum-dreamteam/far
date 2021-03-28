aeb3525b493a3dc5b7a216e59ad87cd4fdcc20e4
DIgis <digismc@DIgiss-MacBook-Pro.local>
2021-03-28 13:06:13 +0300

    some config setups

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Header from '../src/components/Header'
+
+export default function Home() {
+  return (
+    <div className="home">
+      
+      <Header/>
+
+    </div>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from  './Header.module.scss';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      eto ya
+    </header>
+  )
+}
