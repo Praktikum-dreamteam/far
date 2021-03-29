21675b7c9c845a7b4df8f564c71bed3c847fe87b
tttatttu <33tt33@mail.ru>
2021-03-29 23:20:00 +0300

    start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
-import Header from '../src/components/Header'
+import Header from '../src/components/Header';
 import Donate from "../src/components/Donate";
+import Body from '../src/components/Body';
 
 export default function Home() {
-  return (
+  // @ts-ignore
+    return (
     <div className="home">
 
       <Header />
       <Donate />
+      <Body />
     </div>
   )
 }
---
+++ src/components/Body/index.tsx
@@ -0,0 +1,42 @@
+import styles from './Body.module.scss';
+import Radio from "../Radio";
+
+export default function Body() {
+    return (
+        <section className={styles.body}>
+            <div className={styles.body__container}>
+                <div className={styles.body__title}>
+                    <p>Фонд содействия защите здоровья и социальной справедливости имени Андрея Рылькова</p>
+                </div>
+                <div className={styles.body__img} />
+            </div>
+            <div className={styles.body__contant}>
+                <div className={styles.body__contant_span}>e </div>
+                <div>
+                    <h2>Наша миссия:</h2>
+                    <h3> способствовать развитию наркополитики, основанной
+                        на гуманности, терпимости, защите здоровья, достоинства и прав человека.</h3>
+                    <p>
+                        Работа ФАР строится на принципах горизонтального управления и развития проектов на основе инициатив людей, употребляющих наркотики, активистов и активисток, профессионалов и профессионалок в области общественного
+                        здравоохранения и защиты прав
+                        человека.
+                    </p>
+                    <a><img /></a>
+                </div>
+                <div>
+                    <h2>Итоги нашей работы в 2020</h2>
+                    <div>slider</div>
+                    <div>
+                        <img />
+                        <div>
+                            <h3>Получили помощь </h3>
+                            <p>уличной социальной службы ФАР: чистые шприцы, презервативы, тесты на ВИЧ, поддержку и направления</p>
+                        </div>
+                    </div>
+                    <button>Получить помощь</button>
+                </div>
+
+            </div>
+        </section>
+    )
+}
